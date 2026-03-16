8c1af0bf56aa38596ad2f7c0c9f32ee5e06b2b7e
shell <shellbig.ryo@outlook.jp>
2026-03-16 14:00:34 +0900

    fix: 🐛 XieProtocol.cs の定数宣言の位置揃えスペースを除去して dotnet format を通過させる

--- xinputedge-sender/protocol/XieProtocol.cs
+++ xinputedge-sender/protocol/XieProtocol.cs
@@ -4,16 +4,16 @@ namespace XInputEdge
 {
     public static class XieProtocol
     {
-        public const ushort XIE_MAGIC        = 0x5849;  // "XI"
-        public const byte   XIE_VERSION      = 1;
-        public const byte   XIE_TYPE_GAMEPAD = 1;
-        public const int    XIE_PACKET_SIZE  = 22;
+        public const ushort XIE_MAGIC = 0x5849; // "XI"
+        public const byte XIE_VERSION = 1;
+        public const byte XIE_TYPE_GAMEPAD = 1;
+        public const int XIE_PACKET_SIZE = 22;
 
         // flags
         public const byte XIE_FLAG_INPUT_DROP = 0x1;
-        public const byte XIE_FLAG_OVERFLOW   = 0x2;
-        public const byte XIE_FLAG_HEARTBEAT  = 0x4;
-        public const byte XIE_FLAG_RESERVED   = 0x8;
+        public const byte XIE_FLAG_OVERFLOW = 0x2;
+        public const byte XIE_FLAG_HEARTBEAT = 0x4;
+        public const byte XIE_FLAG_RESERVED = 0x8;
 
         public static byte MakeTypeFlags(byte type, byte flags)
         {
@@ -26,7 +26,7 @@ namespace XInputEdge
     {
         public ushort magic;
         public byte version;
-        public byte typeAndFlags;  // type(4bit) | flags(4bit)
+        public byte typeAndFlags; // type(4bit) | flags(4bit)
         public ushort sampleId;
         public uint timestampUs;
         public short lx, ly, rx, ry;
